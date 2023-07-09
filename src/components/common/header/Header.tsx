7ae2587d3aca834aaf36ba724ba15af59ea1f116
Manh Quan <manhquan.05012002@gmail.com>
2023-07-09 22:41:21 +0700

    fix issues

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,32 +1,26 @@
 import Logo from '~/components/logo';
 import LinkItem from './parts/LinkItem';
 import { links } from './parts/mockData';
+import MenuMobile from './parts/MenuMobile';
 
 import logoImageUrl from '~/assets/images/space-neo-logo.png';
 
 import './style.scss';
-import MenuMobile from './parts/MenuMobile';
-
-const PcLayour = true;
 
 const Header = () => {
   return (
     <div className={`header`}>
       <Logo src={logoImageUrl}></Logo>
 
-      {PcLayour ? (
-        <ul className='navigate'>
-          {links.map((link) => (
-            <li>
-              <LinkItem key={link.id} href={link.href}>
-                {link.title}
-              </LinkItem>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <MenuMobile></MenuMobile>
-      )}
+      <ul className='navigate'>
+        {links.map((link) => (
+          <li key={link.id}>
+            <LinkItem href={link.href}>{link.title}</LinkItem>
+          </li>
+        ))}
+      </ul>
+
+      <MenuMobile></MenuMobile>
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,4 @@
-import { useEffect } from 'react';
 const Home = () => {
-  useEffect(() => {
-    document.title = 'Home page';
-  }, []);
-
   return <div>home page</div>;
 };
 
