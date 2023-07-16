d2a5b2362549761de40de36e8667db233b3746cc
Manh Quan <manhquan.05012002@gmail.com>
2023-07-16 18:56:08 +0700

    apply component with storyBook

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,18 +1,23 @@
-import { ReactNode } from 'react';
+import { ReactNode, HTMLAttributes } from 'react';
 
 type type = 'primary' | 'secondary';
 
-interface IButton {
+interface IButton extends HTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   type?: type;
   className?: string;
 }
 
-const Button = ({ children, className = 'button-primary', type = 'primary' }: IButton) => {
+const Button = ({
+  children,
+  className = 'button-primary',
+  type = 'primary',
+  ...props
+}: IButton) => {
   const handleClick = () => {};
 
   return (
-    <button className={`button button-${type} ${className} `} onClick={handleClick}>
+    <button className={`button button-${type} ${className} `} onClick={handleClick} {...props}>
       {children}
     </button>
   );
---
+++ src/stories/components/Header.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Header from '~/components/common/header/Header';
+
+import { withRouter } from 'storybook-addon-react-router-v6';
+
+import { linkHeaders } from '~/mockData';
+
+const meta = {
+  title: 'Space:neo/Header',
+  component: Header,
+  decorators: [withRouter],
+  tags: ['autodocs']
+} satisfies Meta<typeof Header>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const header: Story = {
+  args: {
+    links: linkHeaders
+  }
+};
